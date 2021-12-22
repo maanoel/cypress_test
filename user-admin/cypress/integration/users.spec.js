50a3f6b1982686ee77a004901fc0d5a1bd3f521e
maanoel <manoelvitorbrito@gmail.com>
2021-12-21 22:31:14 -0300

    CYPRESS ╔ MUITO TOP.

--- user-admin/cypress/integration/users.spec.js
+++ user-admin/cypress/integration/users.spec.js
@@ -1,6 +1,14 @@
 describe("Gestão de usuários", () => {
   it("Criar um novo usuário", () => {
+    //Abrir formulario cadastro
     cy.visit("#/users");
     cy.get("a[aria-label=Create").click();
+
+    //preencher o formulário
+    cy.get("#name").type("Joyn Doe");
+    cy.get("#email").type("John does.com");
+
+    //Enviar o formulário
+    cy.get("button[type=submit]").click();
   });
 });
